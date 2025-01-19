c67bc81c56e0af7e7c204144f02262df623836f5
pan-l <zhen_yu_@outlook.com>
2025-01-19 14:10:07 +0800

    Refactor main.py to run FastAPI server and add IDF routes
    - Updated main.py to initialize the FastAPI server using idf_routes instead of the previous server module.
    - Introduced idf_routes.py with endpoints for managing IDF files, including retrieval, creation, and updating of records in the database.
    - Added CORS middleware to allow cross-origin requests.
    - Implemented data processing and simulation functionality within the new routes.
    - Created a new utility file ep_simulation.py for future simulation-related features.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from backend.src.utils.upload_data import Database_Operation
-from backend.src.server import *
+from backend.src.routers.idf_routes import *
 import uvicorn
 
 if __name__ == "__main__":
     # db_op = Database_Operation()
     # db_op.upload_all()
-    uvicorn.run("backend.src.server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.src.routers.idf_routes:app", host="0.0.0.0", port=8000, reload=True)
